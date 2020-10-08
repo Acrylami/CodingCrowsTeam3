ca30295c9b11229d85b16129e1ecd8737e6ef9ce
Reda Tawfik <redatawfik@google.com>
2020-10-08 22:47:35 +0200

    Add data class

---
+++ app/src/main/java/com/app/courseplan/model/Course.java
@@ -0,0 +1,63 @@
+package com.app.courseplan.model;
+
+import java.util.Date;
+
+public class Course {
+    private int id;
+    private String courseName;
+    private Date startDate;
+    private String endDate;
+    private String description;
+
+    public Course() {
+    }
+
+    public Course(int id, String courseName, Date startDate, String endDate,
+            String description) {
+        this.id = id;
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
