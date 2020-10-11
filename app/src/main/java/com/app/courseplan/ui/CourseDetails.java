7b27148b9682a020adb1c1d2a662a5336eeaf5e3
nkosi-tauro <nkosilati23@yahoo.com>
2020-10-11 21:15:47 +0200

    added date-picker

--- app/src/main/java/com/app/courseplan/ui/CourseDetails.java
+++ app/src/main/java/com/app/courseplan/ui/CourseDetails.java
@@ -1,10 +1,16 @@
 package com.app.courseplan.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.app.courseplan.DatabaseHelper;
@@ -15,6 +21,9 @@ public class CourseDetails extends AppCompatActivity {
     Button cancelButton;
     Button saveButton;
     EditText courseTitle, courseURL, courseDescription, startDate, endDate;
+//    Date Picker
+    private DatePickerDialog.OnDateSetListener startDateSetListener, endDateSetListener;
+
 
 
     @Override
@@ -28,7 +37,7 @@ public class CourseDetails extends AppCompatActivity {
         courseURL = findViewById(R.id.courseURL);
         courseDescription = findViewById(R.id.courseDescription);
         saveButton = findViewById(R.id.saveButton);
-
+// Save functionality
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,10 +50,7 @@ public class CourseDetails extends AppCompatActivity {
             }
         });
 
-
-
-
-
+//        Cancel Button
         cancelButton = findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +59,66 @@ public class CourseDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+//Date Picker Dialogs and Listeners
+//        Start Date
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CourseDetails.this,
+                        R.style.MyDatePickerStyle,
+                        startDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+//        End Date
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CourseDetails.this,
+                        R.style.MyDatePickerStyle,
+                        endDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+
+        startDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//              months counts from zero ie January = 0 & Dec = 11
+                month = month +1;
+                String date = month + "/" + day + "/" + year;
+                startDate.setText(date);
+            }
+        };
+        endDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//              months counts from zero ie January = 0 & Dec = 11
+                month = month +1;
+                String date = month + "/" + day + "/" + year;
+                endDate.setText(date);
+            }
+        };
+
+
     }
 
 
