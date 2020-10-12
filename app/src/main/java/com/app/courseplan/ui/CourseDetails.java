7765fabcb13fb24f3c00e8ab8a59e28ba14588ef
Nkosilathi Tauro <nkosilati23@yahoo.com>
2020-10-13 00:30:47 +0200

    added course-details-functionality (#6)
    * added course-details-functionality
    
    * added date-picker

--- app/src/main/java/com/app/courseplan/ui/CourseDetails.java
+++ app/src/main/java/com/app/courseplan/ui/CourseDetails.java
@@ -2,15 +2,124 @@ package com.app.courseplan.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import com.app.courseplan.DatabaseHelper;
 import com.app.courseplan.R;
 
 public class CourseDetails extends AppCompatActivity {
+
+    Button cancelButton;
+    Button saveButton;
+    EditText courseTitle, courseURL, courseDescription, startDate, endDate;
+//    Date Picker
+    private DatePickerDialog.OnDateSetListener startDateSetListener, endDateSetListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+
+        courseTitle = findViewById(R.id.courseTitle);
+        startDate = findViewById(R.id.startDate);
+        endDate = findViewById(R.id.endDate);
+        courseURL = findViewById(R.id.courseURL);
+        courseDescription = findViewById(R.id.courseDescription);
+        saveButton = findViewById(R.id.saveButton);
+// Save functionality
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper myDB = new DatabaseHelper(CourseDetails.this);
+                myDB.addCourse(courseTitle.getText().toString().trim(),
+                        startDate.getText().toString().trim(),
+                        endDate.getText().toString().trim(),
+                        courseURL.getText().toString().trim(),
+                        courseDescription.getText().toString().trim());
+            }
+        });
+
+//        Cancel Button
+        cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CourseDetails.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+//Date Picker Dialogs and Listeners
+//        Start Date
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CourseDetails.this,
+                        R.style.MyDatePickerStyle,
+                        startDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+//        End Date
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CourseDetails.this,
+                        R.style.MyDatePickerStyle,
+                        endDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+
+        startDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//              months counts from zero ie January = 0 & Dec = 11
+                month = month +1;
+                String date = month + "/" + day + "/" + year;
+                startDate.setText(date);
+            }
+        };
+        endDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//              months counts from zero ie January = 0 & Dec = 11
+                month = month +1;
+                String date = month + "/" + day + "/" + year;
+                endDate.setText(date);
+            }
+        };
+
+
     }
+
+
 }
--- app/src/main/java/com/app/courseplan/ui/MainActivity.java
+++ app/src/main/java/com/app/courseplan/ui/MainActivity.java
@@ -1,19 +1,76 @@
 package com.app.courseplan.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.app.courseplan.CustomAdapter;
+import com.app.courseplan.DatabaseHelper;
 import com.app.courseplan.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
+
+    DatabaseHelper myDB;
+    ArrayList<String> course_id, course_title, course_startdate, course_enddate, course_url, course_description;
+    CustomAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recyclerview);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CourseDetails.class);
+                startActivity(intent);
+            }
+        });
 
+        myDB = new DatabaseHelper(MainActivity.this);
+        course_id = new ArrayList<>();
+        course_title = new ArrayList<>();
+        course_startdate = new ArrayList<>();
+        course_enddate = new ArrayList<>();
+        course_url = new ArrayList<>();
+        course_description = new ArrayList<>();
+
+        StoreDataInArrays();
+        customAdapter = new CustomAdapter(MainActivity.this, course_id, course_title, course_url);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
+
+    void StoreDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT ).show();
+        }
+        else{
+            while(cursor.moveToNext()){
+                course_id.add(cursor.getString(0));
+                course_title.add(cursor.getString(1));
+                course_startdate.add(cursor.getString(2));
+                course_enddate.add(cursor.getString(3));
+                course_url.add(cursor.getString(4));
+                course_description.add(cursor.getString(5));
+
+            }
+        }
+    }
+
 
 }
