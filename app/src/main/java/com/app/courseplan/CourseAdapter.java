5c3466ac0c24ce51c904cbad7e6bc3f51701f8f9
Arlyn <Acrylami@gmail.com>
2020-10-13 22:08:15 +0100

    Courses have background colours
    Couses have background colours according to their ID based on this colour scheme: https://coolors.co/ffa69e-faf3dd-b8f2e6-aed9e0-5e6472

--- app/src/main/java/com/app/courseplan/CourseAdapter.java
+++ app/src/main/java/com/app/courseplan/CourseAdapter.java
@@ -1,12 +1,15 @@
 package com.app.courseplan;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.courseplan.model.Course;
@@ -14,7 +17,7 @@ import com.app.courseplan.model.Course;
 import java.util.List;
 
 public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.MyViewHolder> {
-
+    private int colour = 0xFFFFFFFF;
     private Context context;
     private List<Course> mCourses;
     private final CourseAdapterOnClickHandler mClickHandler;
@@ -44,6 +47,32 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.MyViewHold
         holder.courseId.setText(String.valueOf(course.getId()));
         holder.courseTitle.setText(String.valueOf(course.getCourseName()));
         holder.courseUrl.setText(String.valueOf(course.getCourseUrl()));
+
+        //Find colour
+        if (course.getId() % 10 == 1 || course.getId() % 10 == 6) {
+            colour = 0XFFFFA69E;
+        }
+        else if (course.getId() % 10 == 2 || course.getId() % 10 == 7) {
+            colour = 0XFFFAF3DD;
+        }
+        else if (course.getId() % 10 == 3 || course.getId() % 10 == 8) {
+            colour = 0XFFB8F2E6;
+        }
+        else if (course.getId() % 10 == 4 || course.getId() % 10 == 9) {
+            colour = 0XFFAED9E0;
+        }
+        else if (course.getId() % 10 == 5 || course.getId() % 10 == 0) {
+            colour = 0XFF5E6472;
+            //Also set text colour
+            holder.courseTitle.setTextColor(Color.WHITE);
+            holder.courseId.setTextColor(Color.WHITE);
+            holder.courseUrl.setTextColor(0xffeeeeee);
+        }
+        //Background colour
+        //itemView.findViewById(R.id.constraint_course).setBackgroundColor(colour);
+        holder.courseContainer.setBackgroundColor(colour);
+
+        Log.e("==========", course.getCourseName() + " and colour: " + colour);
     }
 
     @Override
@@ -61,12 +90,15 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.MyViewHold
         private TextView courseId;
         private TextView courseTitle;
         private TextView courseUrl;
+        ConstraintLayout courseContainer;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             courseTitle = itemView.findViewById(R.id.course_title_txt);
             courseId = itemView.findViewById(R.id.course_id_txt);
             courseUrl = itemView.findViewById(R.id.course_url_txt);
+
+            courseContainer = itemView.findViewById(R.id.constraint_course);
             itemView.setOnClickListener(this);
         }
 
